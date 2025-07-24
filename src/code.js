9d8e5b5a3681ed6c2c21f32fa9c6422cdc8ea84b
KremasLagos <stathxmkourtis@gmail.com>
2025-07-24 13:21:45 +0300

    Refactored to use class. Need work on code to make sure the code is now modular and have seoeration of concerns.

--- src/code.js
+++ src/code.js
@@ -4,126 +4,143 @@ const closeSvg =
 const myLibrary = [];
 const container = document.querySelector(".container");
 
-function Book(title, arthor, pages, haveRead) {
-  this.title = title;
-  this.arthor = arthor;
-  this.pages = pages;
-  this.haveRead = haveRead;
-  this.id = crypto.randomUUID();
-}
-
-function addBookToLibrary(library, book) {
-  library.push(book);
-}
-function clearForm() {
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("pages").value = "";
-  document.getElementById("haveRead").value = false;
-}
-function buildLibrary() {
-  clearLibrary();
-  for (let i = 0; i < myLibrary.length; i++) {
-    const card = document.createElement("div");
-    container.appendChild(card);
-    card.classList.add(
-      "size-60",
-      "border-1",
-      "border-amber-950",
-      "bg-[var(--color-main)]",
-      "rounded-3xl",
-      "p-4",
-      "grid",
-      "shadow-xl",
-      "grid-cols-1",
-      "grid-rows-4",
-    );
-    let tittle = document.createElement("div");
-    card.appendChild(tittle);
-    tittle.textContent = "Title: " + myLibrary[i].title;
-    let arthor = document.createElement("div");
-    card.appendChild(arthor);
-    arthor.textContent = "Arthor: " + myLibrary[i].arthor;
-    let pages = document.createElement("div");
-    card.appendChild(pages);
-    pages.textContent = "Pages: " + myLibrary[i].pages;
-    let readContainer = document.createElement("div");
-    card.appendChild(readContainer);
-    readContainer.classList.add("flex", "self-start");
-    let read = document.createElement("div");
-    readContainer.appendChild(read);
-    if (myLibrary[i].haveRead) {
-      read.textContent = "Read";
-    } else {
-      read.textContent = "Not Read";
-    }
-    readContainer.insertAdjacentHTML(
-      "beforeend",
-      '<input name="haveRead" id="haveRead" type="checkbox">',
-    );
-    let readCheck = readContainer.lastElementChild;
-    readCheck.classList.add("ml-4");
-    if (myLibrary[i].haveRead == true) {
-      readCheck.checked = true;
-    } else {
-      readCheck.checked = false;
-    }
-    readCheck.addEventListener("input", () => {
-      if (myLibrary[i].haveRead == true) {
-        myLibrary[i].haveRead = false;
-        buildLibrary();
+const displayController = {
+  clearForm() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("haveRead").value = false;
+  },
+  buildLibrary() {
+    this.clearLibrary();
+    for (let i = 0; i < myLibrary.length; i++) {
+      const card = document.createElement("div");
+      container.appendChild(card);
+      card.classList.add(
+        "size-60",
+        "border-1",
+        "border-amber-950",
+        "bg-[var(--color-main)]",
+        "rounded-3xl",
+        "p-4",
+        "grid",
+        "shadow-xl",
+        "grid-cols-1",
+        "grid-rows-4",
+      );
+      let tittle = document.createElement("div");
+      card.appendChild(tittle);
+      tittle.textContent = "Title: " + myLibrary[i].title;
+      let arthor = document.createElement("div");
+      card.appendChild(arthor);
+      arthor.textContent = "Arthor: " + myLibrary[i].arthor;
+      let pages = document.createElement("div");
+      card.appendChild(pages);
+      pages.textContent = "Pages: " + myLibrary[i].pages;
+      let readContainer = document.createElement("div");
+      card.appendChild(readContainer);
+      readContainer.classList.add("flex", "self-start");
+      let read = document.createElement("div");
+      readContainer.appendChild(read);
+      if (myLibrary[i].haveRead) {
+        read.textContent = "Read";
       } else {
-        myLibrary[i].haveRead = true;
-        buildLibrary();
+        read.textContent = "Not Read";
       }
-    });
+      readContainer.insertAdjacentHTML(
+        "beforeend",
+        '<input name="haveRead" id="haveRead" type="checkbox">',
+      );
+      let readCheck = readContainer.lastElementChild;
+      readCheck.classList.add("ml-4");
+      if (myLibrary[i].haveRead == true) {
+        readCheck.checked = true;
+      } else {
+        readCheck.checked = false;
+      }
+      readCheck.addEventListener("input", () => {
+        if (myLibrary[i].haveRead == true) {
+          myLibrary[i].haveRead = false;
+          buildLibrary();
+        } else {
+          myLibrary[i].haveRead = true;
+          buildLibrary();
+        }
+      });
 
-    card.insertAdjacentHTML("beforeend", closeSvg);
-    const close = card.lastElementChild;
-    close.classList.add(
-      "h-7",
-      "w-7",
-      "text-[var(--color-backround)]",
-      "hover:text-[var(--color-text)]",
-    );
-    close.addEventListener("click", () => {
-      console.log(i);
-      console.log(myLibrary[i]);
-      card.remove();
-      myLibrary.splice(i, 1);
-      buildLibrary();
-      console.log(card);
-    });
+      card.insertAdjacentHTML("beforeend", closeSvg);
+      const close = card.lastElementChild;
+      close.classList.add(
+        "h-7",
+        "w-7",
+        "text-[var(--color-backround)]",
+        "hover:text-[var(--color-text)]",
+      );
+      close.addEventListener("click", () => {
+        console.log(i);
+        console.log(myLibrary[i]);
+        card.remove();
+        myLibrary.splice(i, 1);
+        buildLibrary();
+        console.log(card);
+      });
+    }
+  },
+  clearLibrary() {
+    document.querySelector(".container").textContent = "";
+  },
+};
+
+
+const library = (function () {
+  class BookClass {
+    constructor(title, arthor, pages, haveRead) {
+      this.title = title;
+      this.arthor = arthor;
+      this.pages = pages;
+      this.haveRead = haveRead;
+    }
   }
-}
-function clearLibrary() {
-  document.querySelector(".container").textContent = "";
-}
-let book1 = new Book("Hobbit", "Tolkien", 300, true);
-let book2 = new Book("lotr", "Tolkien", 300, false);
-addBookToLibrary(myLibrary, book1);
-addBookToLibrary(myLibrary, book2);
+  function addBookToLibrary(library, book) {
+    library.push(book);
+  }
 
-const openForm = document.querySelector(".openform");
-const dialog = document.querySelector(".dialog");
+  function pageInit() {
+    let book1 = new BookClass("Hobbit", "Tolkien", 300, true);
+    let book2 = new BookClass("lotr", "Tolkien", 300, false);
+    addBookToLibrary(myLibrary, book1);
+    addBookToLibrary(myLibrary, book2);
 
-openForm.addEventListener("click", () => {
-  dialog.showModal();
-});
+  }
 
-document.querySelector("#addbook").addEventListener("click", () => {});
-const form = document.querySelector("#addbookform");
-const addbookbutton = document.querySelector("#addbook");
-addbookbutton.addEventListener("click", (e) => {
-  e.preventDefault();
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let haveRead = document.getElementById("haveRead").checked;
+  const openForm = document.querySelector(".openform");
+  const dialog = document.querySelector(".dialog");
 
-  addBookToLibrary(myLibrary, new Book(title, author, pages, haveRead));
-  buildLibrary();
-  dialog.close();
-  clearForm();
-});
-buildLibrary();
+  openForm.addEventListener("click", () => {
+    dialog.showModal();
+  });
+
+  // document.querySelector("#addbook").addEventListener("click", () => {});
+  const form = document.querySelector("#addbookform");
+  const addbookbutton = document.querySelector("#addbook");
+  addbookbutton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let haveRead = document.getElementById("haveRead").checked;
+
+    addBookToLibrary(myLibrary, new BookClass(title, author, pages, haveRead));
+    displayController.buildLibrary();
+    dialog.close();
+    displayController.clearForm();
+  });
+  pageInit()
+  displayController.buildLibrary();
+  // return {
+  //   myLibrary,
+  // }
+})();
+
+
+
